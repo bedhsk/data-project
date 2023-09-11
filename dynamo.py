cf41a0010ad21682779c315de9536654eb0cad24
Brian Eduardo Batz Enriquez <brianbatz03@gmail.com>
2023-09-11 02:28:52 -0600

    comentarios TODO agregados

--- dynamo.py
+++ dynamo.py
@@ -4,8 +4,10 @@ from datetime import datetime
 # Insertar datos en DynamoDb
 class Dynamo:
     def __init__(self) -> None:
+        # TODO ->> Cambiar el nombre de la tabla (Table(xxxxxx)) y/o agregar más tablas
         self.demo_table = resource('dynamodb').Table('demo-dynamo-python') # acceso a la tabla
 
+    # TODO ->> Modificar esta sentencia para que encaje con la tabla
     def insert(self, customer_id: str, order_id: str, status_name: str):
         print(f'demo_insert')
         response = self.demo_table.put_item(
