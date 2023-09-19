e077df044e38db79090ba774522a108bf0d20111
Brian Eduardo Batz Enriquez <brianbatz03@gmail.com>
2023-09-18 23:28:49 -0600

    obtención de datos del archivo csv

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+import csv
+
 from modules.mongo import Mongo
 from modules.dynamo import Dynamo
 from modules.mariadb import MariaDB
@@ -5,6 +7,14 @@ from modules.mariadb import MariaDB
 dynamo_base = Dynamo()
 maria_base = MariaDB()
 mongo_base = Mongo()
+
+def get_data():
+    with open('data.csv', newline='') as data:
+        lector = csv.reader(data, delimiter=';', quotechar='"')
+        for row in lector:
+            print("Sexo: ", row[0], "otro: ", row[1])
+
+get_data()
 
 # # !INSERT DATA DYNAMO
 # for i in range(50):
@@ -15,7 +25,7 @@ mongo_base = Mongo()
 #     maria_base.insert(f"cus-10{i}", f"ord-10{i}", "pending")
 # maria_base.close_connections()
 
-# !INSERT DATA MONGODB
-for i in range(50):
-    mongo_base.insert(f"cus-10{i}", f"ord-10{i}", "pending")
-mongo_base.close_connection()
+# # !INSERT DATA MONGODB
+# for i in range(50):
+#     mongo_base.insert(f"cus-10{i}", f"ord-10{i}", "pending")
+# mongo_base.close_connection()
