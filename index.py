cd08c76a960f00cf140530c5de295f201f3bb061
Carlos Cancinos <carloscancinos12@gmail.com>
2023-09-19 00:39:38 -0600

    no se que ponerle

--- index.py
+++ index.py
@@ -4,14 +4,19 @@ from modules.mongo import Mongo
 from modules.dynamo import Dynamo
 from modules.mariadb import MariaDB
 
-#dynamo_base = Dynamo()
-#maria_base = MariaDB()
+dynamo_base = Dynamo()
+maria_base = MariaDB()
 mongo_base = Mongo()
 
 def get_data():
     with open('data.csv', newline='') as data:
         lector = csv.reader(data, delimiter=';', quotechar='"')
         for i, row in enumerate(lector):
+            # # !INSERT DATA MARIADB
+            maria_base.insert(i, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12])
+            # # !INSERT DATA DYNAMO
+            dynamo_base.insert(i, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12])
+            # # !INSERT DATA MONGODB
             mongo_base.insert(i, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12])
 
 get_data()
--- modules/mongo.py
+++ modules/mongo.py
@@ -28,7 +28,7 @@ class Mongo:
             print("Error de conexión" + connectionError)
 
     # TODO ->> Modificar esta sentencia para añadir datos
-    def insert(self, id: int, sex: str, age: int, height: int, weight: int, sight_left: float, sight_right: float, SBP: float, DBP: float, BLDS: float, tot_chole: float, gamma_GTP: float, SMK_stat_type_cd:float, DRK_YN: str):
+    def insert(self, id: int, sex: str, age: int, height: int, weight: int, sight_left: float, sight_right: float, SBP: float, DBP: float, BLDS: float, tot_chole: float, gamma_GTP: float, SMK_stat_type_cd:float, DRK_YN: bool):
         try:
             document = {
                 "id": id,  # partition key
