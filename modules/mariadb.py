aecbf26fb864416c4d6cd1a6fd0fad9dbd0b6b5b
Brian Eduardo Batz Enriquez <brianbatz03@gmail.com>
2023-09-24 20:26:54 -0600

    Inserción SQL en MariaDB completada

--- modules/mariadb.py
+++ modules/mariadb.py
@@ -20,24 +20,24 @@ class MariaDB:
                DRK_YN: str,):
         # !INSERT DATA MARIADB
         # TODO ->> Cambiar las sentencias de insert
-        sql = "INSERT INTO Medidas (sex, Age, Height, Weight, SightLeft, SightRight, SBP, DBP, BLDS, Tot_Chole, GammaGTP, SMKStartTypeCd, DRK_YN) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-        values = (
-            sex,
-            age,
-            height,
-            weight,
-            sight_left,
-            sight_right,
-            SBP,
-            DBP,
-            BLDS,
-            tot_chole,
-            gamma_GTP,
-            SMK_stat_type_cd,
-            DRK_YN,
-        )
 
+        # insertar pacientes
+        sql = "INSERT INTO Paciente (Sex, Age) VALUES (%s, %s)"
+        values = (sex, age)
         self.cursor.execute(sql, values)  # ejecutar
+
+        last_id = self.cursor.lastrowid
+
+        # insertar medidas
+        sql = "INSERT INTO Medidas (Height, Weight, SightLeft, SightRight, Paciente_id) VALUES (%s, %s, %s, %s, %s);"
+        values = (height, weight, sight_left, sight_right, last_id)
+        self.cursor.execute(sql, values)
+
+        # insertar datos médicos
+        sql = "INSERT INTO DatosMedicos (SBP, DBP, BLDS, Tot_Chole, Gamma_GTP, SMKStartTypeCd, DRK_YN, Paciente_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (SBP, DBP, BLDS, tot_chole,
+                  gamma_GTP, SMK_stat_type_cd, DRK_YN, last_id)
+        self.cursor.execute(sql, values)
 
         self.connection.commit()  # confirmar cambios
 
